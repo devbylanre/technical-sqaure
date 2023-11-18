a16087534fc11e12fd92a42b52ab78ec8bdeabcb
devbylanre <devbylanre>
2023-11-18 04:26:23 +0100

    redesigned the course post

--- src/components/templates/Post.tsx
+++ src/components/templates/Post.tsx
@@ -1,9 +1,8 @@
 import Heading from '../ui/typo/Heading';
 import {
   RiChatHistoryLine,
-  RiCursorLine,
-  RiChat1Line,
-  RiHeart3Line,
+  RiFlashlightLine,
+  RiBookmarkLine,
   RiAttachment2,
 } from 'react-icons/ri';
 import Badge from '../ui/Badge';
@@ -14,26 +13,31 @@ import Paragraph from '../ui/typo/Paragraph';
 export const Post = () => {
   return (
     <div className='flex flex-col w-full col-span-4 p-3 bg-white border rounded-lg shadow-sm border-zinc-200 gap-y-3'>
-      <Heading className='font-bold'>
+      <Heading className='text-base font-bold md:text-lg'>
         Building a Saas project using MERN stack
       </Heading>
 
       <div className='inline-flex justify-between w-full'>
         <Badge isVisible>
-          <RiChatHistoryLine className='w-4 h-4' />
+          <RiChatHistoryLine className='w-3.5 h-3.5' />
           2h 10m
         </Badge>
         <Badge
           variant='success'
           isVisible
         >
-          <RiCursorLine className='w-4 h-4' />
+          <RiFlashlightLine className='w-3.5 h-3.5' />
           MERN
         </Badge>
       </div>
 
       <div>
-        <Heading className='text-sm font-semibold'>About this course</Heading>
+        <Paragraph className='text-sm'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet in
+          saepe quaerat optio blanditiis possimus nihil atque veniam, aut unde
+          libero soluta assumenda! Velit molestiae quis quia, voluptates quo
+          facere.
+        </Paragraph>
       </div>
 
       <div className='inline-flex items-center justify-between'>
@@ -55,17 +59,7 @@ export const Post = () => {
             variant='outline'
             className='rounded-full'
           >
-            <RiChat1Line className='w-4 h-4' />
-            <span>12</span>
-          </Button>
-
-          <Button
-            size='xs'
-            type='button'
-            variant='outline'
-            className='rounded-full'
-          >
-            <RiHeart3Line className='w-4 h-4' />
+            <RiBookmarkLine className='w-3.5 h-3.5' />
             <span>5</span>
           </Button>
 
@@ -75,7 +69,7 @@ export const Post = () => {
             variant='outline'
             className='rounded-full'
           >
-            <RiAttachment2 className='w-4 h-4' />
+            <RiAttachment2 className='w-3.5 h-3.5' />
             <span>2</span>
           </Button>
         </div>
